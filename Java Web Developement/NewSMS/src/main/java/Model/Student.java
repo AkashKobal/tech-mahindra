064eb40bed9abfb54df955c53332948869a2f052
Akash <akashkobal02@gmail.com>
2025-02-28 23:36:55 +0530

    class update

--- Java Web Developement/NewSMS/src/main/java/Model/Student.java
+++ Java Web Developement/NewSMS/src/main/java/Model/Student.java
@@ -3,17 +3,14 @@ package Model;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class Student implements Serializable {
-	/**
-	 * 
-	 */
+public class Student implements Serializable, Comparable<Student> {  
 	private static final long serialVersionUID = 1L;
 	private String sid;
 	private String sname;
 	private String email;
 	private String address;
 	private int year;
-	//alt shift s
+
 	public String getSid() {
 		return sid;
 	}
@@ -44,37 +41,46 @@ public class Student implements Serializable {
 	public void setYear(int year) {
 		this.year = year;
 	}
+
+	// Implement the compareTo method to sort by sid
+	@Override
+	public int compareTo(Student other) {
+		return this.sid.compareTo(other.sid);
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(address, email, sid, sname, year);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
+		if (obj == null || getClass() != obj.getClass())
 			return false;
 		Student other = (Student) obj;
-		return Objects.equals(address, other.address) && Objects.equals(email, other.email)
-				&& Objects.equals(sid, other.sid) && Objects.equals(sname, other.sname) && year == other.year;
+		return Objects.equals(address, other.address) && 
+			   Objects.equals(email, other.email) &&
+			   Objects.equals(sid, other.sid) && 
+			   Objects.equals(sname, other.sname) && 
+			   year == other.year;
 	}
+
 	@Override
 	public String toString() {
-		return "Student [sid=" + sid + ", sname=" + sname + ", email=" + email + ", address=" + address + ", year="
-				+ year + "]";
+		return "Student [sid=" + sid + ", sname=" + sname + ", email=" + email + ", address=" + address + ", year=" + year + "]";
 	}
+
 	public Student(String sid, String sname, String email, String address, int year) {
-		super();
 		this.sid = sid;
 		this.sname = sname;
 		this.email = email;
 		this.address = address;
 		this.year = year;
 	}
-	public Student() {
-		// TODO Auto-generated constructor stub
-	}
 
+	public Student() {
+		// Default constructor
+	}
 }
